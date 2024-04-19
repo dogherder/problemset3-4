cb8e31e096b96ceb8779c6d58430215a96c29cd9
dogherder <smhong@gmail.com>
2024-04-18 19:10:27 -0700

    Update web_app_1.js

--- web_app_1.js
+++ web_app_1.js
@@ -2,13 +2,21 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://dbuser2:dbUser123@cluster0.toauqqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 	  
 console.log('hey')
-  MongoClient.connect(url, function(err, mydb) {
+
+function main() {
+	
+    MongoClient.connect(url, function(err, mydb) {
+        if(err) { console.log(err); }
     
-  if(err) { console.log(err); }
-  else {
-    var dbo = mydb.db("problemset3-4");
+        var dbo = mydb.db("problemset3-4");
 	var collection = dbo.collection('places');
-    console.log("Success!");
-	  mydb.close();
-  }
-});
+	var newData = {"city": "Medford", "zipcode": "02145"};
+	await collection.insertOne(newData, function(err, res) {
+              if (err) { return console.log(err); } 
+               console.log("new document inserted");
+        });
+    mydb.close();
+    });
+}
+
+main();
