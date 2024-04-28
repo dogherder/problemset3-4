42f52a2413353a919f08b470ce28faf521c883ab
dogherder <smhong@gmail.com>
2024-04-28 10:27:47 -0700

    Update web_app_1.js

--- web_app_1.js
+++ web_app_1.js
@@ -1,22 +1,50 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "";
-	  
-console.log('hey')
+const url = "mongodb+srv://dbuser2:dbUser123@cluster0.toauqqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const readline = require('readline');
+const fs = require('fs');
+global.outputArray = [];	  
 
-function main() {
-	
-    MongoClient.connect(url, function(err, mydb) {
-        if(err) { console.log(err); }
-    
-        var dbo = mydb.db("problemset3-4");
-	var collection = dbo.collection('places');
-	var newData = {"city": "Medford", "zipcode": "02145"};
-	await collection.insertOne(newData, function(err, res) {
-              if (err) { return console.log(err); } 
-               console.log("new document inserted");
-        });
-    mydb.close();
+function readFileAndCreateArray(fileName) {
+  var i = 0;
+  var myFile = readline.createInterface({
+    input: fs.createReadStream(fileName)
+  });
+
+  myFile.on('line', function(line) { 
+    var lineArray = line.split(",");
+    if (i == 0) {
+      city = lineArray[0];
+      zipcodeArray = [];
+      zipcodeArray.push(lineArray[1]);
+      console.log('New city ' + lineArray[0] + ' has been added with a zip code of ' + lineArray[1]);
+    } else {
+      if (lineArray[0] == city) {
+        zipcodeArray.push(lineArray[1]);
+        console.log('City ' + city + ' is being updated with the additional zip code ' + lineArray[1]);
+      } else {
+        //insert create new document
+        var newData = {"city": city, "zipcode": zipcodeArray};
+        global.outputArray.push(newData);
+        //redefine city with new city and start a new zip array
+        city = lineArray[0];
+        zipcodeArray = [];
+        zipcodeArray.push(lineArray[1]);
+        console.log('New city ' + lineArray[0] + ' has been added with a zip code of ' + lineArray[1]);
+      }
+    }
+    i++;
+  });
+  myFile.on('close', async function() {
+    var newData = {"city": city, "zipcode": zipcodeArray};
+    global.outputArray.push(newData);
+    MongoClient.connect(url, async function(err, db) {
+      if (err) { console.log(err); }
+      var dbo = db.db("problemset3-4");
+      var collection = dbo.collection('places');  
+      await collection.insertMany(global.outputArray);
+      db.close();
     });
+  })
 }
 
-main();
+readFileAndCreateArray('zips.csv');
