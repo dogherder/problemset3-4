d71ae87322e8f646cb174ab4d724881312cb4a49
dogherder <smhong@gmail.com>
2024-04-28 13:48:53 -0700

    Update web_app_1.js

--- web_app_1.js
+++ web_app_1.js
@@ -1,9 +1,9 @@
-var http = require('http');
+var https = require('https');
 var url = require('url');
 var port = process.env.PORT || 3000;
 //var port = 8080;   //uncomment to run local
 console.log("This goes to the console window");
-http.createServer(function (req, res) {
+https.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   urlObj = url.parse(req.url,true)
   id = urlObj.query.id
