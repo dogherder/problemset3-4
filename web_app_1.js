3fb2f2a422170f6cb1d29473bea89fa2d20c22f2
dogherder <smhong@gmail.com>
2024-04-28 13:09:49 -0700

    Update web_app_1.js

--- web_app_1.js
+++ web_app_1.js
@@ -15,7 +15,7 @@ http.createServer(function (req, res) {
 }).listen(port);
 
 const MongoClient = require('mongodb').MongoClient;
-url = "mongodb+srv://dbuser2:dbUser123@cluster0.toauqqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// url = "mongodb+srv://dbuser2:dbUser123@cluster0.toauqqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // const readline = require('readline');
 // const fs = require('fs');
 // global.outputArray = [];	  
